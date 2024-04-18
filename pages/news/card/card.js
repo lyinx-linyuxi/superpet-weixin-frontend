c1ec1ce8d82bbd37d98e218545fa25d70be53269
lyinx <lyinx123@163.com>
2024-04-18 17:05:56 +0800

    finish cards display

--- pages/news/card/card.js
+++ pages/news/card/card.js
@@ -2,27 +2,33 @@ const app = getApp()
 Page({
   data: {
     spinShow: true,
-    pets: [
-      {
-        name: "冯曜",
-        age: "3岁",
-        weight: "5kg",
-        description: "活泼爱跳",
-        imageUrl: "https://i.loli.net/2017/08/21/599a521472424.jpg"
-      },
-      {
-        name: "杜博东",
-        age: "18岁",
-        weight: "6kg",
-        description: "yeg",
-        imageUrl: "./img/bg.jpg"
-      },
-      // 你可以添加更多宠物的信息
-    ]
+    pets: []
   },
   onLoad (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    
+    var that = this;
+    wx.request({
+      url: 'http://localhost:8080', // 替换成后端提供的API地址
+      method: 'GET', // 根据后端要求选择使用GET或POST
+      success(res) {
+        if (res.data.code === 1) {
+          that.setData({
+            pets: res.data.data
+          });
+        } else {
+          wx.showToast({
+            title: '数据加载失败',
+            icon: 'none'
+          });
+        }
+      },
+      fail() {
+        wx.showToast({
+          title: '请求失败',
+          icon: 'none'
+        });
+      }
+    });
   },
   onReady () {
     // 页面渲染完成
