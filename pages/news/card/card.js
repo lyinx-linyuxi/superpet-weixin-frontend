5d2eeaafaaf1a97356f2c0f3ba0f11d8e5cef9f4
lyinx <lyinx123@163.com>
2024-04-18 16:54:45 +0800

    xou

--- pages/news/card/card.js
+++ pages/news/card/card.js
@@ -1,7 +1,24 @@
 const app = getApp()
 Page({
   data: {
-    spinShow: true
+    spinShow: true,
+    pets: [
+      {
+        name: "冯曜",
+        age: "3岁",
+        weight: "5kg",
+        description: "活泼爱跳",
+        imageUrl: "https://i.loli.net/2017/08/21/599a521472424.jpg"
+      },
+      {
+        name: "杜博东",
+        age: "18岁",
+        weight: "6kg",
+        description: "yeg",
+        imageUrl: "./img/bg.jpg"
+      },
+      // 你可以添加更多宠物的信息
+    ]
   },
   onLoad (options) {
     // 页面初始化 options为页面跳转所带来的参数
